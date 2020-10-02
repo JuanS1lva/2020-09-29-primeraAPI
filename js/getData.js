854b718f833ab00fa8dae4ac8ef75a1e2cc47d6c
JuanS1lva <juan.silva.vane@gmail.com>
2020-10-02 11:01:08 -0500

    Muestra de pokemons random

--- js/getData.js
+++ js/getData.js
@@ -1,22 +1,67 @@
-const randomsPokemons = Math.floor(Math.random() * 30)
-let pokemones
+let API;
+let randomsPokemons;
 
-const API = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset='+randomsPokemons;
+const randomPokemon = ()=>{
+  randomsPokemons = Math.floor(Math.random() * 100);
+  API = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset='+randomsPokemons;
+}
+let pokemons = {};
 
-
-const getData = (api)=>{
+  const getData = (api)=>{
   return fetch(api)
-    .then( (response) =>response.json())
+    .then((response)=>response.json())
     .then((json)=>{
-      console.log('json-->', json);
+      let html=""
+      if(!!json){
+        html=""
+        json.results.forEach((pj) => {
+          getImg(pj.url)
+          .then((response)=> {
+            html += '<div class="col-3">'
+              html += '<div class="card m-2">'
+              html += `<img src="${response}" class="card-img-top" alt="...">`
+                html += '<div class="card-body" >'
+                html += ` <h5 class="card-title">${pj.name}</h5>`
+                /* html += `<p class="card-text">Status: ${pj.status}</p>`
+                html += `<p class="card-text">Especie: ${pj.species}</p>`
+                html += `<p class="card-text">Genero: ${pj.gender}</p>` */
+                html += '</div>'
+              html += '</div>'
+            html += '</div>'
+            return html
+          })
+          .then((response)=> {
+            document.getElementById("datosPersonaje").innerHTML= response
+          })
+          .catch( (error) => {
+            console.log('error', error);
+            return null;
+          })
+
+      });
+      
+
+      }
       //llenarDatos(json),
       //paginacion(json.info)
     })  
     .catch( (error) => {
       console.log('error', error);
+      return null;
     })
-}
+  }
 
+  const getImg = (api)=>{
+    return fetch(api)
+      .then((response)=>response.json())
+      .then((json)=>{
+        return json.sprites.front_default
+      })  
+      .catch( (error) => {
+        console.log('error', error);
+        return null;
+      })
+    }
 
 const llenarDatos = (data)=>{
   let html = ""
@@ -36,11 +81,9 @@ const llenarDatos = (data)=>{
   document.getElementById("datosPersonaje").innerHTML= html
 }
 
-const paginacion = (data)=>{
-  let html = ''
-  html += `<li class="page-item ${!data.prev?'disabled':''}"><a class="page-link" onclick="getData('${data.prev}')">previous</a></li>`
-  html += `<li class="page-item ${!data.next?'disabled':''}"><a class="page-link" onclick="getData('${data.next}')">next</a></li>`
-  document.getElementById("paginacion").innerHTML=html
-  }
-
-getData(API)
+const paginacion = ()=>{
+  randomPokemon();
+  getData(API);
+}
+randomPokemon();
+getData(API);
